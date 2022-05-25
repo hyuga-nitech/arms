98acc01712c33fa26f30a843e8f6934909a8c834
hyuga-nitech <clz13213@ict.nitech.ac.jp>
2022-05-25 12:17:42 +0900

    create ExManager and Finish

---
+++ python/ExManager.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------------
+# Author:          Hyuga Suzuki
+# Original Author: Takayoshi Hagiwara (KMD)
+# Created:         2022/5/25
+# Summary:         Experiment manager
+# -----------------------------------------------------------------
+
+from RobotControlManager.RobotControlManager import RobotControlManager
+
+if __name__ == '__main__':
+    robotControlManager = RobotControlManager()
+    print('\n----- End program: ExManager.py -----')
